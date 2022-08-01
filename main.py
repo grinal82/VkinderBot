cf675aac94e4b80f04a20cb3bab0f204c688dae0
girnal82 <grinal82@gmail.com>
2022-08-01 23:37:08 +0300

    attempt to connect DB

--- main.py
+++ main.py
@@ -3,74 +3,46 @@ import os
 from dotenv import load_dotenv
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from VKbot import VkBot
-import datetime
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import orm
+from Database import models
+from Database.logic_db import create_tables
 
-td = datetime.datetime.now().date()
 
-
-def write_msg(user_id, message):
+def write_msg(user_id, message, keyboard=None):
     """
     Функция write_msg получает id пользователя ВК <user_id>, 
     которому оно отправит сообщение и собственно само сообщение
     random_id - уникальный идентификатор, 
     предназначенный для предотвращения повторной отправки одинакового сообщения. 
     """
+    post = {
+        'user_id': user_id,
+        'message': message,
+        'random_id': random.randint(0, 2048)
+    }
+    if keyboard != None:
+        post["keyboard"] = keyboard.get_keyboard()
+    else:
+        post = post
+    vk.method('messages.send', post)
+
+
+#  функция чтобы отправлять фото
+def send_photo(user_id, url):
     vk.method(
         'messages.send', {
             'user_id': user_id,
-            'message': message,
+            'attachment': url,
             'random_id': random.randint(0, 2048)
         })
 
 
-def info_on_city(user_id):
-
-    info = vk.method('users.get', {
-        'user_id': user_id,
-        'fields': 'city',
-    })
-    city = info[0]['city']['title']
-    print(f'Пользователь из города: {city}')
-
-
-def info_on_sex(user_id):
-
-    info = vk.method('users.get', {
-        'user_id': user_id,
-        'fields': 'sex',
-    })
-    sex = info[0]['sex']
-    print(f'Половая принадлежность пользователя: {sex}')
-
-
-def info_on_age(user_id):
-
-    info = vk.method('users.get', {
-        'user_id': user_id,
-        'fields': 'bdate',
-    })
-    bdate = info[0]['bdate']
-    print(f'Пользователь родился: {bdate}')
-    year = int(bdate.split(".")[2])
-    month = int(bdate.split(".")[1])
-    day = int(bdate.split(".")[0])
-    bd = datetime.date(year, month, day)
-    age_years = int((td - bd).days / 365.25)
-    print(f'Пользователю {age_years} лет')
-
-
-def search_friends():
-    users = vk.method('users.search', {
-        'sort': '0',
-        'city': 'Тула',
-        'sex': '0'
-    })
-    print(users).json()
-
-
 load_dotenv()
-# получаем созданный ранее токен
+# получаем созданный ранее токен группы для работы бота
 token = os.getenv("VK_API_TOKEN")
 
 # Авторизуемся как группа VK
@@ -79,19 +51,31 @@ vk = vk_api.VkApi(token=token)
 # Работа с сообщениями
 longpoll = VkLongPoll(vk)
 
+login = os.getenv("LOGIN")
+password = os.getenv("PASSWORD")
+db_name = os.getenv("DB_NAME")
+
+DSN = f"postgresql://{login}:{password}@localhost:5432/{db_name}"
+
+engine = sqlalchemy.create_engine(DSN)
+DBsession = orm.sessionmaker(bind=engine)
+
+# create_tables(engine)
 print("Server started")
-for event in longpoll.listen():
+with DBsession() as db_sesion:
+    for event in longpoll.listen():
 
-    if event.type == VkEventType.MESSAGE_NEW:
+        if event.type == VkEventType.MESSAGE_NEW:
 
-        if event.to_me:
+            if event.to_me:
 
-            print(f'New message from {event.user_id}', end='')
+                print(f'New message from {event.user_id}', end='')
 
-            bot = VkBot(event.user_id)
-
-            write_msg(event.user_id, bot.new_message(event.text))
-            info_on_city(event.user_id)
-            info_on_sex(event.user_id)
-            info_on_age(event.user_id)
-            search_friends()
+                bot = VkBot(event.user_id, db_sesion)
+                create_tables(engine)
+                keyboard = VkKeyboard()
+                keyboard.add_button("Поехали!", VkKeyboardColor.PRIMARY)
+                text = event.text.lower()
+                write_msg(event.user_id, bot.new_message(event.text), keyboard)
+                bot.search_all(event.user_id)
+                # bot.get_photo()
